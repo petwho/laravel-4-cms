55abc262f51d872af01933f40207cecba0d3f4b1
Tran Khanh <trankhanh.tk.kt@gmail.com>
2014-10-18 10:40:54 +0700

    - Update manual page

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -1,19 +1,28 @@
 @extends('layouts.admin')
 @section('content')
+  <h3>Dashboard</h3>
+  <div class="row-gap-medium"></div>
   <div class="row">
     <div class="col-xs-12">
-      <h3 class="text-muted">
-        Hướng dẫn sử dụng: <br>
-      </h3>
-      <div class="jumbotron">
-      <h3>Xin chào {{$user->username}}!</h3>
-      <p>Để quản lý site vui lòng nhấp vào các menu phía tay trái.
-        Để đăng xuất vui lòng nhấp vào biểu tượng logout phía góc trên bên tay phải.
-      </p>
-      <p>Mục projects dùng để quản lý các dự án trong trang chủ. Bạn có thể thêm, chỉnh sửa và xóa các dự án.</p>
-      <p>Mục post dùng để chỉnh sửa các bài viết xuất hiện trong phần kiến thức xây dựng. Tương tự bạn có thể thêm, sửa, xóa bài viết trong mục này.</p>
-      <p>Để quản lý người dùng click icon biểu tượng người dùng (user) phía góc trên tay phải.</p>
+      <div class="alert alert-white">
+        <h3>Xin chào, <em>{{$user->username}}</em>!</h3>
+        <div class="row">
+          <div class="col-xs-3">
+            <p class="text-muted">Mục projects dùng để quản lý các dự án trong trang chủ. Bạn có thể thêm, chỉnh sửa và xóa các dự án.</p>
+          </div>
+          <div class="col-xs-3 col-xs-offset-1">
+            <p class="text-muted">Mục post dùng để chỉnh sửa các bài viết xuất hiện trong phần kiến thức xây dựng.</p>
+          </div>
+          <div class="col-xs-3 col-xs-offset-1">
+            <p class="text-muted">Để quản lý người dùng click icon biểu tượng người dùng (user) phía góc trên tay phải.</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+  <style type="text/css">
+    body {
+      background-color: #eee;
+    }
+  </style>
 @stop
