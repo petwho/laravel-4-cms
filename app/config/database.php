f6d99d4873751b877202613d2c8c2bac9505700b
Tran Khanh <trankhanh.tk.kt@gmail.com>
2014-10-26 10:23:04 +0700

    Reupdate database config file

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'xuyena_cons',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'abcdabcd',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
