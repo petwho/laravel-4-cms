8b921cff473b391b2a46cffe2c95ef75b0304e4a
Tran Khanh <trankhanh.tk.kt@gmail.com>
2014-11-17 22:04:43 +0700

    Add partners

--- app/views/static_blocks/home/architect.blade.php
+++ app/views/static_blocks/home/architect.blade.php
@@ -1,6 +1,34 @@
-<div class="section block-01">
-  <h2 class="h2-title">KIẾN TRÚC, XÂY DỰNG &amp; NỘI THẤT NHÀ ĐẸP</h2>
-  <h3 class="h3-title mt-10">Thiết kế &amp; Thi công trên toàn quốc </h3>
-  <p class="txt-01 mt-10">Nhà đẹp với phong cách đơn giản và bố trí nội thất thông minh. Ánh sáng tràn ngập với khung cửa kính lớn, thiên về cảnh quan xanh màu lá. Nhà ở, khách sạn, bar, cafe, nhà hàng...Cũng đều mang một nét chung “simple is the best”. Thiết kế bao gồm kiến trúc, nội thất, cảnh quan và quy hoạch đô thị. Xây dựng phần thô, thi công hoàn thiện. Sản xuất, cung cấp, lắp đặt sản phẩm nội thất.</p>
-  <p class="align-center mt-10"><img src="images/index/img_01.jpg" alt="" class="img-box"/></p>
+<div id='partners'>
+    <h2 class='h2-title'>Đối Tác Của Chúng Tôi</h2>
+    <?php
+        $partners = [
+            'http://www.starwoodhotels.com/sheraton/index.html?language=en_US',
+            'http://irishotel.com.vn/',
+            'http://ptn.vn/',
+            'http://www.muongthanh.vn/vi/home',
+            'http://www.vinamilk.com.vn/',
+            'http://www.reetech.com.vn/',
+            'http://mpc.net.vn/',
+        ];
+    ?>
+    @for($i = 0; $i < count($images = glob('../public/images/partners/*')); $i++)
+      <div>
+        <a href={{$partners[$i]}} real="nofollow" target="_blank"><img class="img-thumbnail" src="/images/partners/{{ basename($images[$i]) }}" width=100 height=81 style="width: 90%; height: 90%"></a>
+      </div>
+    @endfor
 </div>
+<div style='clear: both;'></div>
+<style type="text/css">
+#partners div {
+    padding-top: 15px;
+    padding-bottom: 20px;
+    display: inline-block; width: 19%; text-align: center; height: 100px;margin: 0 0;
+}
+#partners a img {
+    border: 1px solid #ddd;
+    background-color: #fff;
+    border-radius: 4px;
+    padding: 4px;
+    /*display: inline-block;*/
+}
+</style>
